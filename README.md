ce4d8b7e0b9074f33ab06a0b96d28f91b0480e13
Giuseppe Bertone <bertone.giuseppe@gmail.com>
2021-12-28 21:02:23 +0100

    Fixed meta data URL; Updated icon; Fixed spelling of 'Sky TG24'

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# Diretta SkyTG24 Kodi plugin
+# Diretta Sky TG24 Kodi plugin
 
-Kodi unofficial plugin to stream Sky Tg24 from the official website.
+Kodi unofficial plugin to stream Sky TG24 from the official website.
 
 ## Installation
 
-Download the contents of the repository as a zip file and then from the add-ons menu select "Install from zip file"
+Download the contents of the repository as a zip file and then from the add-ons menu select "Install from zip file".
--- addon.py
+++ addon.py
@@ -2,19 +2,19 @@ import re
 import xbmcgui
 import requests
 
-url = 'http://videoplatform.sky.it/player/json/get_livestream_1.json'
+url = 'https://apid.sky.it/vdp/v1/getLivestream?id=1'
 res = requests.get(url)
 
 try:
 	streaming_url = 'https://' + re.findall('"streaming_url":"https{0,1}://(.*?)"', res.text)[0]
 	thumb_url = 'https://' + re.findall('"img":"https{0,1}://(.*?)"', res.text)[1]
-	listitem = xbmcgui.ListItem('SkyTG24')
-	listitem.setInfo('video', {'Title': 'Diretta SkyTG24'})
-	listitem.setArt({ 'thumb': thumb_url})
+	listitem = xbmcgui.ListItem('Sky TG24')
+	listitem.setInfo('video', {'Title': 'Diretta Sky TG24'})
+	listitem.setArt({'thumb': thumb_url})
 	xbmc.Player().play(streaming_url, listitem)
 
 except Exception as ex:
 	message = 'Eccezione: %s' % ex
-	listitem = xbmcgui.ListItem('SkyTG24 - Errore')
-	listitem.setInfo('video', {'Title': 'Errore nell\' avvio del plugin SkyTG24'})
+	listitem = xbmcgui.ListItem('Sky TG24 - Errore')
+	listitem.setInfo('video', {'Title': 'Errore nell\'avvio del plugin Sky TG24'})
 	listitem.setInfo('video', { 'plot': message })
